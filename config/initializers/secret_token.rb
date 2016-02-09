7ce2f16506e34a97c7b12bc757b3fdf761b3adeb
Akshai Raj <akshairajm@gmail.com>
2016-02-09 17:53:52 +0530

    Sample commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CocBlackhawks::Application.config.secret_token = '378944d37cc8a2d11ac457f941aa75947b9c3cc53f603bded9750cee06f342cce5f7404d023be7f0f836c4bfb4e8e5898e53f7186b22baa07c05551b3901608d'
